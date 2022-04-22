b58255120514e119ecd20ee4b439520e6959f6c1
rkelly30 <46577129+rkelly30@users.noreply.github.com>
2022-04-22 11:43:49 -0600

    Updated 404 page

---
+++ components/branding/Brand.js
@@ -0,0 +1,15 @@
+    import React from "react"
+
+    import { GiFern } from "react-icons/gi"
+    import { BrandStyles } from "./styles"
+
+    function Brand  (props){
+        return( 
+            <BrandStyles {...props}>
+                <GiFern size={props.iconSize} color={props.fill}/>
+                <figcaption> Robert's Plants</figcaption>
+            </BrandStyles>
+        )
+    }
+
+    export default Brand 
--- pages/404/PageNotFound.js
+++ pages/404/PageNotFound.js
@@ -1,24 +1,11 @@
-import React, { useState} from "react";
+import React from "react";
 
-import { Link } from "react-router-dom";
-
-import { onAuthStateChanged } from "firebase/auth"
-import { auth } from "libs/firebase"
+import Link from "next/link";
 
 import { Brand } from 'components/branding/'
 import { PageNotFoundPageStyles } from "./styles"
 
 function PageNotFound (props){
-    const [isUser, setIsUser] = useState(false)
-
-    onAuthStateChanged(auth, (user)=>{
-        if(user){
-            setIsUser(true)
-        }else{
-            setIsUser(false)
-        }
-    })
-
     return(
         <>
         <PageNotFoundPageStyles>
@@ -29,11 +16,7 @@ function PageNotFound (props){
                 <h1>404 | Page Not Found</h1>
                 <p>We are sorry, but the page you are looking for does not exist.</p>
                 <p>
-                    Please click 
-                    {
-                        isUser? <Link to="/dashboard"> here </Link> : <Link to="/"> here </Link>
-                    }
-                    to return to the website.
+                    Please click<Link to="/"> here </Link>to return to the website.
                 </p>
             </header>
         </PageNotFoundPageStyles>
